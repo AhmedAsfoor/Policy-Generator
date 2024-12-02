b4298480b5f4047e070e64ba0432317b6c653e79
Ahmed Asfoor <ahmedasfoor@microsoft.com>
2024-12-02 13:41:25 -0600

    added ps scripts

---
+++ src/scripts/Get_All_Aliases.ps1
@@ -0,0 +1,20 @@
+$aliases = Get-AzPolicyAlias
+
+$output = $aliases | ForEach-Object {
+    [PSCustomObject]@{
+        ResourceType = "$($_.Namespace)/$($_.ResourceType)"
+        Aliases = $_.Aliases | ForEach-Object {
+            if ($_.Name -and $_.DefaultPath) {
+                # Properly format alias and default path
+                "$($_.Name) ($($_.DefaultPath))"
+            } else {
+                "$($_.Name) (No DefaultPath Found)"
+            }
+        }
+    }
+}
+
+$jsonOutput = $output | ConvertTo-Json -Depth 10 -Compress
+
+$outputFilePath = "./AzPolicyAliases.json"
+$jsonOutput | Out-File -FilePath $outputFilePath -Encoding utf8
